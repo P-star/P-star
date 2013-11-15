9a53cfea36769169f44f2649b3c7e190af79854d
Atle Solbakken <atle@goliathdns.no>
2013-11-15 16:12:23 +0100

    Added logo to documentation

--- docs/docbook/generate_html.sh
+++ docs/docbook/generate_html.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xsltproc  --output  /var/www/html/default/pstar/docbook.html /usr/share/xml/docbook/stylesheet/docbook-xsl/xhtml/chunk.xsl source.xml
+mkdir /var/www/html/default/pstar/images/ 2>&1 > /dev/null
+cp -f ../../logo/logo.png /var/www/html/default/pstar/images/
