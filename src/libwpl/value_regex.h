de6078d3f24434efb8979d938e69fd6b7aeefc70
Atle Solbakken <atle@goliathdns.no>
2013-11-21 02:04:35 +0100

    Added regex value files (previously forgotten)

---
+++ src/libwpl/value_regex.h
@@ -0,0 +1,56 @@
+/*
+
+-------------------------------------------------------------
+
+Copyright (c) MMXIII Atle Solbakken
+atle@goliathdns.no
+
+-------------------------------------------------------------
+
+This file is part of P* (P-star).
+
+P* is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+P* is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with P*.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------
+
+*/
+
+#pragma once
+
+#include "typenames.h"
+#include "value_holder.h"
+#include "exception.h"
+
+#include <boost/regex.hpp>
+
+class wpl_operator_struct;
+
+class wpl_value_regex : public wpl_value {
+	private:
+	boost::regex my_regex;
+
+	public:
+	PRIMITIVE_TYPEINFO(regex)
+	wpl_value_regex(const char *regex_string) :
+		my_regex(regex_string)
+	{}
+
+	int do_operator (
+			wpl_expression_state *exp_state,
+			wpl_value *final_result,
+			const wpl_operator_struct *op,
+			wpl_value *lhs,
+			wpl_value *rhs
+	);
+};
