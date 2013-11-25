d512717c5fde140b0a6b870c68e4dcfe26f05d20
Atle Solbakken <atle@goliathdns.no>
2013-11-25 16:47:06 +0100

    Added test suite script

---
+++ tests/run_tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export QUERY_STRING="a&c=1&d=2&d=3"
+
+WPL="../src/wpl";
+TMP="tmp_test";
+
+if [ ! -f $WPL ]; then
+	echo "Could not find wrapper script for WPL-executable"
+	exit 1
+fi
+
+SOMETHING_WENT_WRONG=0
+
+# Generate .result-files if they don't already exist
+for FILE in `ls *.pstar`; do
+	RESULT_FILE="$FILE.result";
+	if [ ! -f "$RESULT_FILE" ]; then
+		echo -n "Generate $FILE...";
+		$WPL -f $FILE > $RESULT_FILE;
+		if [ "$?" != "0" ]; then
+			echo "Error when generating result file for script '$FILE'"
+			exit 1
+		fi
+		echo "Done";
+	fi
+	echo -n "Checking $FILE...";
+
+	$WPL -f $FILE 2>&1 > $TMP;
+	if [ "$?" != "0" ]; then
+		echo "failed 1";
+		SOMETHING_WENT_WRONG=1
+	else
+		cmp -s $TMP $RESULT_FILE 2>&1 
+		if [ "$?" != "0" ]; then
+			echo "failed 2";
+			SOMETHING_WENT_WRONG=1
+		else
+			echo "OK";
+		fi
+	fi
+done
+
+rm $TMP
+
+exit $SOMETHING_WENT_WRONG
