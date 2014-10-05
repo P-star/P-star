22f6dda0972aa48f74068f1a2fe81baa4675031f
Atle Solbakken <atle@goliathdns.no>
2014-10-05 10:13:41 +0200

    Improve docs index system by pre-generating P* code instead of parsing text file.

--- docs/docbook/install_web.sh
+++ docs/docbook/install_web.sh
@@ -7,6 +7,6 @@ rm parts_tmp/*
 
 mkdir -p /var/www/pstar/docs
 cp -f parts_tmp/ch03* /var/www/pstar/docs
-cp -f ch3_index /var/www/pstar/docs
+cp -f ch3_index.pstar /var/www/pstar/docs
 
 cp -rf parts_tmp/images /var/www/pstar/
