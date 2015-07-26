250f99a94f9818ad2c841966c3314fda570097f4
Atle Solbakken <atle@goliathdns.no>
2015-07-26 19:57:30 +0200

    Remove erroneous throw of wpl_type_end_statement in sql.cpp

--- src/libmysql/sql.cpp
+++ src/libmysql/sql.cpp
@@ -94,8 +94,6 @@ void wpl_sql::parse_value (wpl_namespace *parent_namespace) {
 	sql->load_position(get_position());
 	sql->__parse_value(parent_namespace);
 	load_position(sql->get_position());
-
-	throw wpl_type_end_statement(get_position());
 }
 
 void wpl_sql::__parse_value (wpl_namespace *parent_namespace) {
